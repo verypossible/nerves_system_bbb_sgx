ab7cbe5eafe83ad6880ce5c9e1e36d28ba845ad3
Justin Schneck <jschneck@mac.com>
2020-07-16 16:27:49 -0400

    bb-kmscube: New package
    This is a version of kmcsube specifically designed to work with the PowerVR graphics accelerator.

---
+++ package/bb-kmscube/bb-kmscube.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# bb_kmscube
+#
+################################################################################
+
+BB_KMSCUBE_VERSION = 7bc7ff6c4ead19761003ed4a08d00ce64effe38b
+BB_KMSCUBE_SITE = git://git.ti.com/glsdk/kmscube.git
+BB_KMSCUBE_SITE_METHOD = git
+BB_KMSCUBE_LICENSE = MIT
+BB_KMSCUBE_DEPENDENCIES = libdrm
+
+define BB_KMSCUBE_RUN_AUTOGEN
+	cd $(@D) && PATH=$(BR_PATH) ./autogen.sh
+endef
+
+BB_KMSCUBE_PRE_CONFIGURE_HOOKS += BB_KMSCUBE_RUN_AUTOGEN
+
+$(eval $(autotools-package))
