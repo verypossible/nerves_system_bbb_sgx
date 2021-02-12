1baaa354676c20e62a860d38a22cea38e92e9d4c
Justin Schneck <jschneck@mac.com>
2021-02-12 13:03:04 -0500

    Fix buildroot package dependencies

--- package/bb-kmscube/bb-kmscube.mk
+++ package/bb-kmscube/bb-kmscube.mk
@@ -8,7 +8,7 @@ BB_KMSCUBE_VERSION = 7bc7ff6c4ead19761003ed4a08d00ce64effe38b
 BB_KMSCUBE_SITE = git://git.ti.com/glsdk/kmscube.git
 BB_KMSCUBE_SITE_METHOD = git
 BB_KMSCUBE_LICENSE = MIT
-BB_KMSCUBE_DEPENDENCIES = libdrm
+BB_KMSCUBE_DEPENDENCIES = libdrm bb-ti-sgx-um
 
 define BB_KMSCUBE_RUN_AUTOGEN
 	cd $(@D) && PATH=$(BR_PATH) ./autogen.sh
